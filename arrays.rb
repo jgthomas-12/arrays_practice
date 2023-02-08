f788baff9064134987fa8b0209e03a8cf30f0006
Jesse Thomas <jgthomas3@gmail.com>
2023-02-07 17:11:54 -0700

    Add assignment character to each array

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,7 @@
-nature ["rocks", "wind", "trees", "pine", "water"]
-numbers [5, 234, 345, 23, 57]
-floats [4.56, 34.4, 6.7, 22.22]
-booleans [true, false, false, true]
+nature = ["rocks", "wind", "trees", "pine", "water"]
+numbers = [5, 234, 345, 23, 57]
+floats = [4.56, 34.4, 6.7, 22.22]
+booleans = [true, false, false, true]
+
+nature.pop
+puts nature 
